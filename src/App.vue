7785b1bf03fc0ae8b2a4ad74a180ea56f6e494e3
t3yamoto <3yamoto.dev@gmail.com>
2019-11-28 20:20:03 +0900

    auth step3

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <amplify-authenticator />
     <div id="nav">
       <li><router-link to="/ai">AIアプリ</router-link></li>
       <li><router-link to="/chat">チャットアプリ</router-link></li>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from "./router";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import locale from "element-ui/lib/locale/lang/ja";
+import awsconfig from "./aws-exports";
+import Amplify, * as AmplifyModules from "aws-amplify";
+import { AmplifyPlugin } from "aws-amplify-vue";
+
+Amplify.configure(awsconfig);
+Vue.use(AmplifyPlugin, AmplifyModules);
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI, { locale });
