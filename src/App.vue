f5e3240ce198b259fcd1e9a282089f16f2a3e563
t3yamoto <3yamoto.dev@gmail.com>
2019-11-28 20:30:20 +0900

    auth step5

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <amplify-authenticator />
+    <div v-if="signedIn">
+      <div id="nav">
+        <div class="amplify-signed-out">
+          <amplify-sign-out />
+          ようこそ、{{ username }}さん
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <amplify-authenticator />
+    </div>
     <div id="nav">
       <li><router-link to="/ai">AIアプリ</router-link></li>
       <li><router-link to="/chat">チャットアプリ</router-link></li>
@@ -10,5 +20,40 @@
     </div>
   </div>
 </template>
+
+<script>
+import { Auth } from "aws-amplify";
+import { AmplifyEventBus } from "aws-amplify-vue";
+
+window.LOG_LEVEL = "VERBOSE";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      signedIn: false,
+      username: ""
+    };
+  },
+  async beforeCreate() {
+    try {
+      let cognitoUser = await Auth.currentAuthenticatedUser();
+      this.signedIn = true;
+      this.username = cognitoUser.username;
+    } catch (err) {
+      this.signedIn = false;
+    }
+    AmplifyEventBus.$on("authState", async info => {
+      if (info === "signedIn") {
+        let cognitoUser = await Auth.currentAuthenticatedUser();
+        this.signedIn = true;
+        this.username = cognitoUser.username;
+      } else {
+        this.signedIn = false;
+      }
+    });
+  }
+};
+</script>
 
 <style src="./assets/style.css" />
